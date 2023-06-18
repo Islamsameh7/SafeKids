67d2b0752d7c8e9da0bd0a595305726e1a1066ce
Islam Sameh <islamsameh33@gmail.com>
2023-06-18 21:14:26 +0300

    models

--- Back-end/SafeKids/SafeKids/SafeKids/settings.py
+++ Back-end/SafeKids/SafeKids/SafeKids/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'API',
 ]
 
 MIDDLEWARE = [
@@ -106,6 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'API.CustomUser'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
