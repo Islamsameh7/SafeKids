17f5b6045100d4f93bb9c298a901021b3570ff64
hams-166 <85313312+hams-166@users.noreply.github.com>
2023-06-21 02:13:35 +0300

    Revert "push"
    This reverts commit 21c33410997da7f591a06c70481e8de22af0d06e.

--- Front-end/App.js
+++ Front-end/App.js
@@ -23,7 +23,7 @@ import UserProfile from "./src/components/userFunctions/UserProfile";
 import KidProfile from "./src/components/userFunctions/KidProfile";
 import MyKids from "./src/components/userFunctions/MyKids";
 import MissingKids from "./src/components/userFunctions/MissingKids";
-
+import Notifications from "./src/components/Notifications";
 
 const Stack = createNativeStackNavigator();
 
@@ -42,7 +42,7 @@ function App(props) {
           }}
         >
 
-
+          <Stack.Screen name="Notifications" component={Notifications} />
           <Stack.Screen name="GetStarted" component={GetStarted} />
           <Stack.Screen name="Signup" component={Signup} />
           <Stack.Screen name="Login" component={Login} />
