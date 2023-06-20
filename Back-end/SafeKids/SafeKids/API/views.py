f44a2a6085f020376894b9185c35cfd25b56b249
Islam Sameh <islamsameh33@gmail.com>
2023-06-20 15:16:58 +0300

    login

--- Back-end/SafeKids/SafeKids/API/views.py
+++ Back-end/SafeKids/SafeKids/API/views.py
@@ -8,11 +8,20 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import CustomUserSerializer
+from rest_framework import status
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
-def Login():
-    return True
+@api_view(['POST'])
+def login(request):
+    email = request.data.get('email')
+    password = request.data.get('password')
+    user = authenticate(request, email=email, password=password)
+    if user is not None:
+        login(request, user)
+        return Response(status=status.HTTP_200_OK)
+    return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
 
 @api_view(['POST'])
 def register(request):
